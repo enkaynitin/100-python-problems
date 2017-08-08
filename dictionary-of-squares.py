a0726cdd6fccb6e2f5481ce90845693a694b6be0
Nitin kumar <enkaynitin@github.com>
2017-08-08 17:22:03 +0530

    Adding dictionary of squares

---
+++ dictionary-of-squares.py
@@ -0,0 +1,7 @@
+number = int(raw_input())
+squares = {}
+
+for i in range(1, number+1):
+    squares[i] =  (i*i)
+
+print squares
---
+++ factorial.py
@@ -0,0 +1,10 @@
+number = int(raw_input())
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
+
+print factorial(number)
