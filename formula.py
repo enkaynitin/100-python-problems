b844ef13782b2272d6ff8ab9578aa1defb3d1108
Nitin kumar <enkaynitin@github.com>
2017-08-09 16:40:55 +0530

    Edited formula.py

--- formula.py
+++ formula.py
@@ -2,7 +2,7 @@ import math
 
 C = 50
 H = 30
-calculate = raw_input().split(',')
+calculate = raw_input().split(',')  # raw_input() stores as a string
 result =[]
 
 def Q(args):
@@ -10,8 +10,9 @@ def Q(args):
     D = [d for d in args]
     for d in D:
         d = float(d)
-        c = str(int(math.sqrt(( 2 * C * d)/H)))
+        c = str(int(math.sqrt(( 2 * C * d)/H))) # output of math.sqrt() is
+        # converted to int and then to string as per the required output
         l.append(c)
-    return l
+    return l   # l is list of type string elements
 result = Q(calculate)
 print ','.join(result)
