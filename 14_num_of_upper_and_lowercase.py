dd8d2a1e810de33df07fb69b48e9f21fb05d9e38
Nitin kumar <enkaynitin@github.com>
2017-08-10 22:56:46 +0530

    14_num_of_upper_and_lowercase.py

---
+++ 14_num_of_upper_and_lowercase.py
@@ -0,0 +1,16 @@
+sentence = raw_input()
+d = {
+    "UPPER CASE":0,
+    "LOWER CASE":0
+}
+
+for c in sentence :
+    if c.isupper():
+        d["UPPER CASE"]+=1
+    elif c.islower():
+        d["LOWER CASE"]+=1
+    else:
+        pass
+
+print "UPPER CASE", d["UPPER CASE"]
+print "LOWER CASE", d["LOWER CASE"]
